8d6c51c68173841b675734b2afb286ddd4141553
doche15 <dominik.chevalier.1@ulaval.ca>
2025-01-30 09:06:01 -0500

    Graphiques de densité conditionnelles

--- Exemple_synthese_base.R
+++ Exemple_synthese_base.R
@@ -39,8 +39,6 @@ rho <- 0.001
 ### Modèle avec indépendance et comonotonicité
 sum(dpois(1:15, lam) * (1:15) * (qexp(kappa, bet) + 1 / bet))
 
- sum(dpois(1:15, lam) * (qexp(kappa, bet / k) + 1 / (bet / k)))
-
 ### Modèle avec composantes comonotones
 cbind(0:15, ppois(0:15, lam), 1- ppois(0:15, lam))
 
@@ -63,12 +61,12 @@ antideriv <- function(u) ((u - 1) * log(1 - u) - u)
 k <- 2
 tvarX <- function(k, bet) qexp(k, bet) + 1/bet
 
-1 / (ppois(kk, lam) - ppois(kk - 1, lam)) * ((1 - ppois(k-1, lam)) *
+1 / (ppois(k, lam) - ppois(k - 1, lam)) * ((1 - ppois(k-1, lam)) *
                                                  tvarX(ppois(k-1, lam), bet) -
                                                  (1 - ppois(k, lam)) *
                                                  tvarX(ppois(k, lam), bet))
 
-integrate(function(y) qexp(y, bet), ppois(1, lam), ppois(2, lam))$value / (ppois(kk, lam) - ppois(kk - 1, lam))
+integrate(function(y) qexp(y, bet), ppois(1, lam), ppois(2, lam))$value / (ppois(k, lam) - ppois(k - 1, lam))
 
 
 ## TVARS
@@ -99,3 +97,65 @@ calculer_TVaRS_crm_antimonotones(0.95, function(x) qpois(x, lam),
 calculer_entropique_crm_antimonotones(0.001, function(x) ppois(x, lam),
                                       function(x) qpois(x, lam),
                                       function(x) qexp(x, bet), 15)
+
+##### Densités conditionnelles #####
+### Avec composantes comonotones
+fxsachantn <- function(x, k, lam, bet)
+{
+    dexp(x, bet) / (ppois(k, lam) - ppois(k - 1, lam)) *
+        (qexp(ppois(k - 1, lam), bet) < x) *
+        (qexp(ppois(k, lam), bet) > x)
+}
+
+# Couleurs
+colors <- c("red", "blue", "green", "purple", "orange", "brown", "pink", "turquoise",
+            "darkgreen")
+
+# Première
+curve(fxsachantn(x, 1, lam, bet), xlim = c(0, 1000), n = 1e3, col = colors[1],
+      ylab = "Densité",
+      xlab = "Montant d'un sinistre X",
+      main = "Densité du montant de sinistre X sachant N = k\n(Comonotonicité entre la fréquence et la sévérité)")
+
+# Autres
+for (i in 2:9) {
+    curve(fxsachantn(x, i, lam, bet), xlim = c(0, 1000), n = 1e3, col = colors[i], add = TRUE)
+}
+
+# Légende
+legend_labels <- sapply(1:9, function(i) bquote(f[X|N == .(i)]))
+
+# Légende
+legend("topright", legend = paste0("k = ", 1:9), col = colors, lty = 1, cex = 0.8)
+
+
+### Avec composantes antimonotones
+fxsachantn <- function(x, k, lam, bet)
+{
+    dexp(x, bet) / (ppois(k, lam) - ppois(k - 1, lam)) *
+        (qexp(1 - ppois(k, lam), bet) <= x) *
+        (qexp(1 - ppois(k - 1, lam), bet) > x)
+}
+
+# Couleurs
+colors <- c("red", "blue", "darkgreen", "purple")
+
+# Première
+curve(fxsachantn(x, 4, lam, bet), xlim = c(0, 300), n = 1e4, col = colors[4],
+      ylab = "Densité",
+      xlab = "Montant d'un sinistre X",
+      main = "Densité du montant de sinistre X sachant N = k\n(Antimonotonicité entre la fréquence et la sévérité)",
+      lwd = 2)
+
+# Autres
+for (i in 1:3) {
+    curve(fxsachantn(x, i, lam, bet), xlim = c(0, 300), n = 1e4, col = colors[i],
+          add = TRUE, lwd = 2)
+}
+
+# Légende
+legend_labels <- sapply(1:4, function(i) bquote(f[X|N == .(i)]))
+
+# Légende
+legend("topright", legend = paste0("k = ", 1:4), col = colors, lty = 1, cex = 0.8,
+       lwd =2)
