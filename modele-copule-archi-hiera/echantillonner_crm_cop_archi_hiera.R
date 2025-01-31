c88395e0aab7018e82182b381e5ac039cfe39523
Félix Côté <FECOT24@ulaval.ca>
2025-01-31 10:51:26 -0500

    Update echantillonner_crm_cop_archi_hiera.R
    - Correction de la définition de Theta01
    - Évite de regénéer un nouveau Theta01 pour i=1,...,N

--- modele-copule-archi-hiera/echantillonner_crm_cop_archi_hiera.R
+++ modele-copule-archi-hiera/echantillonner_crm_cop_archi_hiera.R
@@ -8,43 +8,52 @@
 # À tester.
 echantillonner_crm_cop_archi_hiera <- function(n, qDistTheta0, tlsTheta0, qDistB, tlsTheta1, qDistN, qDistX)
 {
-    realisations <- list()
-    realisation_i <- vector(mode = "numeric")
-
-    # Simuler les réalisation de N
-    realisations_N <- qDistN(tlsTheta0(rexp(n) / qDistTheta0(runif(n))))
-
-    R_X <- 0
-    U_X <- 0
-    Theta01 <- 0
-
-    max_N_i <- 0
-
-    for (i in seq(n))
+  realisations <- list()
+  realisation_i <- vector(mode = "numeric")
+  
+  theta0 <- qDistTheta0(runif(n)) # Échantillonner theta0
+  R <- rexp(n) # Échantillonner R
+  U <- tlsTheta0(R / theta0) # Calculer U
+  
+  realisations_N <- qDistN(U) # Simuler les réalisations de N
+  
+  # Initialiser les valeurs
+  R_X <- 0
+  U_X <- 0
+  Theta01 <- 0
+  
+  max_N_i <- 0
+  
+  for (i in seq(n))
+  {
+    # Stocker la réalisation i de N
+    realisation_i[1] <- realisations_N[i]
+    
+    if (realisation_i[1] != 0)
     {
-        # Stocker la réalisation i de N
-        realisation_i[1] <- realisations_N[i]
-
-        if (realisation_i[1] != 0)
-        {
-            for (j in seq(realisations_N[i]))
-            {
-                Theta01 <- sum(qDistB(runif(realisations_N[i])))
-                R_X <- rexp(1)
-                U_X <- tlsTheta1(R_X /Theta01)
-
-                realisation_i <- c(realisation_i, qDistX(U_X))
-            }
-        }
-
-        if (realisation_i[1] > max_N_i)
-        {
-            max_N_i <- realisation_i[1]
-        }
-
-        realisations[[i]] <- realisation_i
+      
+      Theta01 <- sum(qDistB(runif(theta0))) # Échantilonner Theta01
+      
+      # Pour i = 1,...,N
+      for (j in seq(realisations_N[i]))
+      {
+        
+        R_X <- rexp(1) # Échantilonner R_i
+        U_X <- tlsTheta1(R_X / Theta01) # Calculer U_i
+        X_X = qDistX(U_X)
+        
+        realisation_i <- c(realisation_i, X_X) # Créer le vecteur c(N, X_1,...,X_N)
+      }
     }
-
-    list(realisations = realisations,
-         max_N_i = max_N_i)
+    
+    if (realisation_i[1] > max_N_i)
+    {
+      max_N_i <- realisation_i[1]
+    }
+    
+    realisations[[i]] <- realisation_i
+  }
+  
+  list(realisations = realisations,
+       max_N_i = max_N_i)
 }
