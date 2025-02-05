9d30e6e7b808b669c02dd5890069c6b838d15f00
doche15 <dominik.chevalier.1@ulaval.ca>
2025-02-05 10:09:14 -0500

    Ajout de la version somme composée de la fonction pour fs

--- modele-copule-archi/calculer_fS.R
+++ modele-copule-archi/calculer_fS.R
@@ -26,7 +26,6 @@ calculer_fS_archi_simple <- function(nfft, alpha, tlsinvDistTheta, dDistTheta, q
 
   for (j in seq(jmax))
   {
-      #browser()
       # Pr[X | theta]
       fxj <- numeric(nfft)
       fxj[1] <- exp(-j * tlsinvDistTheta(pDistX(0), alpha))
@@ -56,8 +55,51 @@ calculer_fS_archi_simple <- function(nfft, alpha, tlsinvDistTheta, dDistTheta, q
 
 }
 
-# Vérification qu'on somme à 1
 
+# f_{S} (ih)
+calculer_fSfft_archi_simple <- function(nfft, alpha, tlsinvDistTheta, dDistTheta, qDistTheta, pDistX, qDistN, pDistN)
+{
+
+    epsilon = 1/1000000000 # Erreur acceptable
+    jmax = qDistTheta(1 - epsilon, alpha)
+    kmax <- qDistN(1 - epsilon)
+    fS <- numeric(nfft)
+    ii <- 1:(nfft - 1)
+
+    for (j in seq(jmax))
+    {
+        # Pr[X | theta]
+        fxj <- numeric(nfft)
+        fxj[1] <- exp(-j * tlsinvDistTheta(pDistX(0), alpha))
+        fxj[2:(nfft)] <- (exp(-j * tlsinvDistTheta(pDistX(ii), alpha)) -
+                              exp(-j * tlsinvDistTheta(pDistX(ii - 1), alpha)))
+        ffxj <- fft(fxj)
+
+        # Pr[N | theta]
+        fnj <- numeric(kmax + 1)
+        fnj[1] <- exp(-j * tlsinvDistTheta(pDistN(0), alpha))
+        fnj[2:(kmax+1)] <- (exp(-j * tlsinvDistTheta(pDistN(1:kmax), alpha)) -
+                                exp(-j * tlsinvDistTheta(pDistN(1:kmax - 1), alpha)))
+
+        fxk <- matrix(numeric((kmax + 1) * nfft), ncol = kmax + 1)
+        fxk[1, 1] <- fnj[1]  # masse à 0
+        fnj_nfft <- numeric(nfft)
+        fnj_nfft[1:(kmax + 1)] <- fnj
+        fgpn <- sapply(ffxj, function(a) sum(a^(0:(nfft-1)) * fnj_nfft))
+
+        fsj <- Re(fft(fgpn, inverse = TRUE)) / nfft
+        print(round(sum(fsj), 6) == 1)
+        fS <-  fS + dDistTheta(j, alpha) * fsj
+    }
+
+    fS
+
+}
+
+
+
+# Vérification qu'on somme à 1
+source("geom_shifted.R")
 fs <- calculer_fS_archi_simple(nfft = 2^9, alpha = 0.2,
                                                          tlsinvDistTheta = function(x, alpha) log((1-alpha)/x + alpha),
                                                          dDistTheta = function(x, alpha) dgeo_shifted(x, alpha),
@@ -67,3 +109,15 @@ fs <- calculer_fS_archi_simple(nfft = 2^9, alpha = 0.2,
                                                          pDistN = function(x) ppois(x, 2))
 
 sum(fs)
+
+
+fsfft <- calculer_fSfft_archi_simple(nfft = 2^9, alpha = 0.2,
+                               tlsinvDistTheta = function(x, alpha) log((1-alpha)/x + alpha),
+                               dDistTheta = function(x, alpha) dgeo_shifted(x, alpha),
+                               qDistTheta = function(x, alpha) qgeo_shifted(x, alpha),
+                               pDistX = function(x) ppois(x, 10),
+                               qDistN = function(x) qpois(x, 2),
+                               pDistN = function(x) ppois(x, 2))
+
+sum(fs)
+
